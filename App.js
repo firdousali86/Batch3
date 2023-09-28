243f0d8382f2e032030e829d4eb534450fbd06e4
Firdous Ali <firdous.ali.tp@gmail.com>
2023-09-28 18:03:37 +0500

    layout

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
   TextInput,
   ScrollView,
   Image,
+  Platform,
 } from 'react-native';
 
 import {UserProfile, MyUserList} from './components';
@@ -25,50 +26,79 @@ function App() {
   const [lastName, setLastName] = useState('');
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <ScrollView>
-        <TextInput
-          value={firstName}
-          onChangeText={changedText => {
-            setFirstName(changedText);
-          }}
-          style={{backgroundColor: 'yellow', height: 40, margin: 10}}
-        />
-
-        <TextInput
-          value={lastName}
-          onChangeText={changedText => {
-            setLastName(changedText);
-          }}
-          style={{backgroundColor: 'yellow', height: 40, margin: 10}}
-        />
-
-        <UserProfile
-          firstName={firstName}
-          lastName={lastName}
-          city={'Karachi'}
-          country={'Pakistan'}
-          childrenStyle={styles.userProfileStyle}
-          onSubmitPressed={fetchedData => {
-            setUserList([...userList, fetchedData]);
-          }}
-          onDeletePressed={() => {
-            setUserList([]);
-          }}>
-          <Text>first child node</Text>
-          <Text>second child node</Text>
-          <Text>third child node</Text>
-
-          <Image
-            style={styles.imageStyle}
-            source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-          />
-        </UserProfile>
-
-        {userList && userList.length > 0 && <MyUserList userData={userList} />}
-      </ScrollView>
-    </SafeAreaView>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'pink',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+      }}>
+      <Text>{Platform.OS === 'android' ? 'ANDROID' : 'IOS'}</Text>
+      <View
+        style={{
+          width: 80,
+          height: 80,
+          backgroundColor: 'red',
+          alignSelf: 'flex-start',
+        }}></View>
+      <View style={{width: 80, height: 80, backgroundColor: 'yellow'}}></View>
+      <View
+        style={{
+          width: 80,
+          height: 80,
+          backgroundColor: 'green',
+          alignSelf: 'flex-end',
+        }}></View>
+      <Text>sample text</Text>
+    </View>
   );
+
+  // return (
+  //   <SafeAreaView style={{flex: 1}}>
+  //     <ScrollView>
+  //       <TextInput
+  //         value={firstName}
+  //         onChangeText={changedText => {
+  //           setFirstName(changedText);
+  //         }}
+  //         style={{backgroundColor: 'yellow', height: 40, margin: 10}}
+  //       />
+
+  //       <TextInput
+  //         value={lastName}
+  //         onChangeText={changedText => {
+  //           setLastName(changedText);
+  //         }}
+  //         style={{backgroundColor: 'yellow', height: 40, margin: 10}}
+  //       />
+
+  //       <UserProfile
+  //         firstName={firstName}
+  //         lastName={lastName}
+  //         city={'Karachi'}
+  //         country={'Pakistan'}
+  //         childrenStyle={styles.userProfileStyle}
+  //         onSubmitPressed={fetchedData => {
+  //           setUserList([...userList, fetchedData]);
+  //         }}
+  //         onDeletePressed={() => {
+  //           setUserList([]);
+  //         }}>
+  //         <Text>first child node</Text>
+  //         <Text>second child node</Text>
+  //         <Text>third child node</Text>
+
+  //         <Image
+  //           style={styles.imageStyle}
+  //           source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+  //         />
+  //       </UserProfile>
+
+  //       {userList && userList.length > 0 && <MyUserList userData={userList} />}
+  //     </ScrollView>
+  //   </SafeAreaView>
+  // );
 }
 
 export default App;
