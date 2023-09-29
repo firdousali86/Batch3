ef6e24a694f99e07297d25a418c1697884941448
Firdous Ali <firdous.ali.tp@gmail.com>
2023-09-29 14:07:40 +0500

    navigation working well

--- App.js
+++ App.js
@@ -15,16 +15,15 @@ import {
   Image,
   Platform,
 } from 'react-native';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {UserProfile, MyUserList} from './components';
 
 import styles from './style';
 
-function App() {
-  const [userList, setUserList] = useState([]);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+const Stack = createNativeStackNavigator();
 
+const TestFlexScreen = () => {
   return (
     <View
       style={{
@@ -53,52 +52,69 @@ function App() {
       <Text>sample text</Text>
     </View>
   );
+};
 
-  // return (
-  //   <SafeAreaView style={{flex: 1}}>
-  //     <ScrollView>
-  //       <TextInput
-  //         value={firstName}
-  //         onChangeText={changedText => {
-  //           setFirstName(changedText);
-  //         }}
-  //         style={{backgroundColor: 'yellow', height: 40, margin: 10}}
-  //       />
+const TestStatePropsScreen = () => {
+  const [userList, setUserList] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  //       <TextInput
-  //         value={lastName}
-  //         onChangeText={changedText => {
-  //           setLastName(changedText);
-  //         }}
-  //         style={{backgroundColor: 'yellow', height: 40, margin: 10}}
-  //       />
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView>
+        <TextInput
+          value={firstName}
+          onChangeText={changedText => {
+            setFirstName(changedText);
+          }}
+          style={{backgroundColor: 'yellow', height: 40, margin: 10}}
+        />
 
-  //       <UserProfile
-  //         firstName={firstName}
-  //         lastName={lastName}
-  //         city={'Karachi'}
-  //         country={'Pakistan'}
-  //         childrenStyle={styles.userProfileStyle}
-  //         onSubmitPressed={fetchedData => {
-  //           setUserList([...userList, fetchedData]);
-  //         }}
-  //         onDeletePressed={() => {
-  //           setUserList([]);
-  //         }}>
-  //         <Text>first child node</Text>
-  //         <Text>second child node</Text>
-  //         <Text>third child node</Text>
+        <TextInput
+          value={lastName}
+          onChangeText={changedText => {
+            setLastName(changedText);
+          }}
+          style={{backgroundColor: 'yellow', height: 40, margin: 10}}
+        />
 
-  //         <Image
-  //           style={styles.imageStyle}
-  //           source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-  //         />
-  //       </UserProfile>
+        <UserProfile
+          firstName={firstName}
+          lastName={lastName}
+          city={'Karachi'}
+          country={'Pakistan'}
+          childrenStyle={styles.userProfileStyle}
+          onSubmitPressed={fetchedData => {
+            setUserList([...userList, fetchedData]);
+          }}
+          onDeletePressed={() => {
+            setUserList([]);
+          }}>
+          <Text>first child node</Text>
+          <Text>second child node</Text>
+          <Text>third child node</Text>
 
-  //       {userList && userList.length > 0 && <MyUserList userData={userList} />}
-  //     </ScrollView>
-  //   </SafeAreaView>
-  // );
+          <Image
+            style={styles.imageStyle}
+            source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+          />
+        </UserProfile>
+
+        {userList && userList.length > 0 && <MyUserList userData={userList} />}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="testFlex" component={TestFlexScreen} />
+        <Stack.Screen name="testStateProps" component={TestStatePropsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
