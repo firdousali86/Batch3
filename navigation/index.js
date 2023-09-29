0d52a9b477dabec109d093bfe76cd0a70a451ff6
Firdous Ali <firdous.ali.tp@gmail.com>
2023-09-29 17:01:43 +0500

    different stacks

--- navigation/index.js
+++ navigation/index.js
@@ -2,25 +2,51 @@ import {} from 'react';
 import {} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {TestFlexScreen, TestStatePropsScreen} from '../screens';
+import {
+  TestFlexScreen,
+  TestStatePropsScreen,
+  Dashboard,
+  Login,
+} from '../screens';
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="testFlex"
-        component={TestFlexScreen}
-        options={{title: 'Testing Flex System'}}
-      />
-      <Stack.Screen
-        name="testStateProps"
-        component={TestStatePropsScreen}
-        options={{title: 'Test State and Props'}}
-      />
-    </Stack.Navigator>
-  );
+  const getMainStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen
+          name="dashboard"
+          component={Dashboard}
+          options={{title: 'Dashboard'}}
+        />
+        <Stack.Screen
+          name="testFlex"
+          component={TestFlexScreen}
+          options={{title: 'Testing Flex System'}}
+        />
+        <Stack.Screen
+          name="testStateProps"
+          component={TestStatePropsScreen}
+          options={{title: 'Test State and Props'}}
+        />
+      </Stack.Group>
+    );
+  };
+
+  const getAuthStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen
+          name="login"
+          component={Login}
+          options={{title: 'Login'}}
+        />
+      </Stack.Group>
+    );
+  };
+
+  return <Stack.Navigator>{getMainStack()}</Stack.Navigator>;
 };
 
 export default Navigator;
--- screens/index.js
+++ screens/index.js
@@ -1,4 +1,6 @@
 import TestFlexScreen from './TestFlexScreen';
 import TestStatePropsScreen from './TestStatePropsScreen';
+import Dashboard from './Dashboard';
+import Login from './Login';
 
-export {TestFlexScreen, TestStatePropsScreen};
+export {TestFlexScreen, TestStatePropsScreen, Dashboard, Login};
