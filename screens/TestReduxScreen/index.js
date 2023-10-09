2181ed0449ac67984e8da45a0f49b45e4289ec84
Firdous Ali <firdous.ali.tp@gmail.com>
2023-10-09 17:41:02 +0500

    action with arguement

--- screens/TestReduxScreen/index.js
+++ screens/TestReduxScreen/index.js
@@ -1,9 +1,14 @@
-import {} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {useState} from 'react';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
-import {decrement, increment} from '../../features/counter/counterSlice';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from '../../features/counter/counterSlice';
 
 const TestReduxScreen = () => {
+  const [inputval, setinputval] = useState('');
   const dispatch = useDispatch();
   const count = useSelector(state => state.counter.value);
 
@@ -25,6 +30,22 @@ const TestReduxScreen = () => {
           dispatch(decrement());
         }}>
         <Text>Decrement</Text>
+      </TouchableOpacity>
+
+      <TextInput
+        value={inputval}
+        onChangeText={changedText => {
+          setinputval(changedText);
+        }}
+        keyboardType="numeric"
+        style={{height: 40, backgroundColor: 'yellow', padding: 5, margin: 10}}
+      />
+
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(incrementByAmount(parseInt(inputval)));
+        }}>
+        <Text>Increment by value</Text>
       </TouchableOpacity>
     </View>
   );
