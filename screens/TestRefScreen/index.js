e0f0a7da8d39f3cf414a4537e98a1835781aee75
Firdous Ali <firdous.ali.tp@gmail.com>
2023-10-13 14:11:47 +0500

    another sample

--- screens/TestRefScreen/index.js
+++ screens/TestRefScreen/index.js
@@ -3,7 +3,10 @@ import {Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 const TestRefScreen = () => {
   const [inputVal, setInputVal] = useState('');
+  const [inputVal2, setInputVal2] = useState('');
+
   const inputRef = useRef(null);
+  const inputRef2 = useRef(null);
 
   return (
     <View>
@@ -16,11 +19,21 @@ const TestRefScreen = () => {
         placeholder="First Name"
         style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
       />
+      <TextInput
+        ref={inputRef2}
+        value={inputVal2}
+        onChangeText={changedText => {
+          setInputVal2(changedText);
+        }}
+        placeholder="Last Name"
+        style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
+      />
       <TouchableOpacity
         onPress={() => {
           //   setInputVal('this is written from button press');
           //   inputRef.current.clear();
-          inputRef.current.setNativeProps({text: 'this text is set by useref'});
+          //   inputRef.current.setNativeProps({text: 'this text is set by useref'});
+          inputRef2.current.focus();
         }}>
         <Text>Press</Text>
       </TouchableOpacity>
