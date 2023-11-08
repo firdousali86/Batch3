5f77b91f953941429470210cab7857ce26b4edd1
Firdous Ali <firdous.ali.tp@gmail.com>
2023-11-08 16:58:57 +0500

    cryptohelper

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Navigator from './navigation';
-import {PersistanceHelper} from './helpers';
+import {PersistanceHelper, CryptoHelper} from './helpers';
 import {store} from './store';
 import {Provider} from 'react-redux';
 import {AppStateN} from './components';
@@ -17,6 +17,12 @@ function App() {
   useEffect(() => {
     // PersistanceHelper.setValue('testkey', 'testvalue');
     PersistanceHelper.setObject('testkey', {a: 'b', c: 'd', e: 'f'});
+
+    const encryptedString = CryptoHelper.encryptString(
+      'hey this is plain text',
+    );
+
+    console.log(encryptedString);
   }, []);
 
   return (
