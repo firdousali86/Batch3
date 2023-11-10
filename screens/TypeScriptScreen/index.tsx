4ba4cda981987b87141d5b795cc7f62031880b09
Firdous Ali <firdous.ali.tp@gmail.com>
2023-11-10 15:03:09 +0500

    test cases

--- screens/TypeScriptScreen/index.tsx
+++ screens/TypeScriptScreen/index.tsx
@@ -23,8 +23,17 @@ export default function TypeScriptScreen() {
     phone?: string | number;
   };
 
+  type Person2 = {
+    fullName: string;
+    email: string;
+    phoneNo?: string | number;
+    xyz?: any;
+  };
+
+  type CommonPerson = Person | Person2; //union
+
   // let person: {name: string; age: number; courses: string[]; phone: string};
-  let person: Person;
+  let person: CommonPerson;
 
   person = {
     name: 'firdous ali',
@@ -39,7 +48,7 @@ export default function TypeScriptScreen() {
   //   courses: string[];
   //   phone?: string | number; //union
   // }[];
-  let people: Person[];
+  let people: CommonPerson[];
 
   people = [
     {
@@ -47,13 +56,31 @@ export default function TypeScriptScreen() {
       age: 23,
       courses: ['sdf', 'dfdf', 'gfg'],
       phone: '5645454',
+      xyz: 'sdfgsdf',
     },
-    {name: 'sdfsdfsdf', age: 23, courses: ['sdf', 'dfdf', 'gfg']},
+    {name: 'sdfsdfsdf', age: 23, courses: ['sdf', 'dfdf', 'gfg'], xyz: 345345},
     {
       name: 'sdfsdfsdf',
       age: 23,
       courses: ['sdf', 'dfdf', 'gfg'],
-      phone: 456454,
+      phone: undefined,
+      xyz: true,
+    },
+    {
+      fullName: 'ssdsdf df',
+      email: 'ddfgdf@dfdf.df',
+      phoneNo: '34534534',
+      xyz: [],
+    },
+    {fullName: ' fghfg fh ', email: 'dfgdf@gffg.gg', name: 'hgfh rt r'},
+    {name: 'sdfds sdfsd', age: 55},
+    {fullName: ' sdf sdfds '},
+    {
+      name: 'dfdf dgd ffg',
+      xyz: 'f gdfg d',
+      fullName: 'DSSDF',
+      email: 'sdsdf',
+      abc: 4554,
     },
   ];
 
