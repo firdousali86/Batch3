bab67373febd1a7b7c3217ce4d0b8677c50c9c43
Firdous Ali <firdous.ali.tp@gmail.com>
2023-11-13 15:04:40 +0500

    sample rtl and jest tests

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,13 @@ import UserProfile from './UserProfile';
 import CustomComponent from './CustomComponent';
 import MapControl from './MapControl';
 import AppStateN from './AppStateN';
+import ReactTestComponent from './ReactTestComponent';
 
-export {MyUserList, UserProfile, CustomComponent, MapControl, AppStateN};
+export {
+  MyUserList,
+  UserProfile,
+  CustomComponent,
+  MapControl,
+  AppStateN,
+  ReactTestComponent,
+};
